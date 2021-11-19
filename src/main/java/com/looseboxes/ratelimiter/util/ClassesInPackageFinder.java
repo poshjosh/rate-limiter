35956f754aa6bdeeacc01a0c265fa15243df0fb4
Chinomso Ikwuagwu <chinomso.ikwuagwu@mercateo.com>
2021-11-19 17:23:12 +0100

    Add method which takes multiple packages

--- src/main/java/com/looseboxes/ratelimiter/util/ClassesInPackageFinder.java
+++ src/main/java/com/looseboxes/ratelimiter/util/ClassesInPackageFinder.java
@@ -1,8 +1,19 @@
 package com.looseboxes.ratelimiter.util;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @FunctionalInterface
 public interface ClassesInPackageFinder {
+
+    default List<Class<?>> findClasses(List<String> packageNames, ClassFilter classFilter) {
+        Objects.requireNonNull(classFilter);
+        return packageNames.isEmpty() ? Collections.emptyList() : packageNames.stream()
+                .flatMap(packageName -> findClasses(packageName, classFilter).stream())
+                .collect(Collectors.toList());
+    }
+
     List<Class<?>> findClasses(String packageName, ClassFilter classFilter);
 }
