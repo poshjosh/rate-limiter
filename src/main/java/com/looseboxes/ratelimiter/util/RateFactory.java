7f51277fa4f2361be6c43409bcd2c2657038e117
chinomso <posh.bc@gmail.com>
2021-11-25 10:42:41 +0100

    Remove unused classes

--- src/main/java/com/looseboxes/ratelimiter/util/RateFactory.java
+++
@@ -1,9 +0,0 @@
-package com.looseboxes.ratelimiter.util;
-
-import com.looseboxes.ratelimiter.annotation.RateComposition;
-
-import java.util.List;
-
-public interface RateFactory<K> {
-    List<RateComposition<K>> getRates();
-}
